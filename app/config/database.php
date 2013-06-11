511f63c203aecca87d8b0e96bde470066380f81f
timothy-r <tim.rodger@gmail.com>
2013-06-11 22:19:50 +0100

    set database name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'stats',
 			'username'  => 'root',
 			'password'  => '3ipdoodle',
 			'charset'   => 'utf8',
